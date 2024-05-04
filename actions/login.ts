695779415475894ee9db41278341c3708ccd3e8d
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2024-05-05 00:00:35 +0300

    feat: - Add Next Auth - Add New database models

---
+++ actions/login.ts
@@ -0,0 +1,118 @@
+'use server';
+
+import * as z from 'zod';
+
+import { AuthError } from 'next-auth';
+
+import { signIn } from '@/auth';
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { sendVerificationEmail, sendTwoFactorTokenEmail } from '@/lib/mail';
+import {
+  generateTwoFactorToken,
+  generateVerificationToken,
+} from '@/lib/tokens';
+import { db } from '@/lib/db';
+import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
+
+export const login = async (
+  values: z.infer<typeof LoginSchema>,
+  callbackUrl?: string
+) => {
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: 'Invalid fields' };
+  }
+
+  const { email, password, code } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser?.email || !existingUser?.password) {
+    return { error: 'Email does not exist!' };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerificationEmail(existingUser.email, verificationToken.token);
+
+    return { success: 'Confirmation email sent!' };
+  }
+
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+    if (code) {
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+
+      if (!twoFactorToken) {
+        return { error: 'Invalid two factor token!' };
+      }
+
+      if (twoFactorToken.token !== code) {
+        return { error: 'Invalid two factor token!' };
+      }
+
+      const hasExpired = new Date(twoFactorToken.expiresAt) < new Date();
+
+      if (hasExpired) {
+        return { error: 'Two factor token has expired!' };
+      }
+
+      await db.twoFactorToken.delete({
+        where: {
+          id: twoFactorToken.id,
+        },
+      });
+
+      const existingConfirmation = await getTwoFactorConfirmationByUserId(
+        existingUser.id
+      );
+
+      if (existingConfirmation) {
+        await db.twoFactorConfirmation.delete({
+          where: {
+            id: existingConfirmation.id,
+          },
+        });
+      }
+
+      await db.twoFactorConfirmation.create({
+        data: {
+          userId: existingUser.id,
+        },
+      });
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+
+      await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
+
+      return { twoFactor: true };
+    }
+  }
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+    });
+
+    return { success: '' };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid credentials!' };
+        default:
+          return { error: 'Something went wrong!' };
+      }
+    }
+
+    throw error;
+  }
+};
