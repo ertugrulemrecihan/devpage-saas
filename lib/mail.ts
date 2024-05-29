32b34cc996690ddb3ff98ea71832ff237f7a5079
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2024-05-29 16:42:26 +0300

    fix prisma error

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,12 +2,14 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export const sendVerificationEmail = async (
   email: string,
   token: string,
   callbackUrl?: string
 ) => {
-  let confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  let confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   if (callbackUrl) {
     confirmLink += `&callbackUrl=${encodeURIComponent(callbackUrl)}`;
@@ -22,7 +24,7 @@ export const sendVerificationEmail = async (
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
