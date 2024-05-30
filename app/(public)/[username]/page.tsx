96be597a7cf75dab09b74688d550f3b59aaa6c46
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2024-05-30 20:03:09 +0300

    feat: Add line animation in edit mode border

--- app/(public)/[username]/page.tsx
+++ app/(public)/[username]/page.tsx
@@ -15,7 +15,7 @@ import { cn } from '@/lib/utils';
 
 import Image from 'next/image';
 import { BeatLoader } from 'react-spinners';
-import { toast, useToast } from '@/components/ui/use-toast';
+import { toast } from '@/components/ui/use-toast';
 import { ProfileBackground } from '@/components/profile-background';
 import { profileBackgroundColors } from '@/utils/profile-background-colors';
 import { InlineEdit } from '@/components/inline-edit';
@@ -176,207 +176,220 @@ const PublicUserPage = () => {
       className={cn(
         'w-full h-full min-h-screen transition-all duration-300 overflow-x-hidden',
         {
-          'p-4': isPageEditing,
+          'p-4 flex items-center justify-center': isPageEditing,
         }
       )}
     >
       <div
-        className={cn(
-          'w-full h-full bg-[#FAFAFA] transition-all duration-300',
-          {
-            'border-4 border-[#A855F7] ring-8 ring-[#FAFAFA] rounded-xl':
-              isPageEditing,
-          }
-        )}
+        className={cn('w-full h-full relative overflow-hidden', {
+          'rounded-xl shadow-edit-mode p-1': isPageEditing,
+        })}
       >
-        <div className="w-full relative">
-          <AnimatePresence>
-            {isPageEditing && (
-              <motion.div
-                initial={{ top: -20, opacity: 0 }}
-                animate={{ top: 0, opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.2 }}
-                className="absolute left-0 z-50 w-full flex justify-center"
-              >
-                <div className="bg-[#A855F7] rounded-b-lg flex items-center justify-center px-4 py-[3px]">
-                  <span className="text-xs text-white font-medium">
-                    Edit Mode
-                  </span>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
+        {isPageEditing && (
+          <>
+            <div className="edit-mode-line top-right z-[-1]"></div>
+            <div className="edit-mode-line top-left z-[-1]"></div>
+            <div className="edit-mode-line right-line z-[-1]"></div>
+            <div className="edit-mode-line left-line z-[-1]"></div>
+            <div className="edit-mode-line bottom-right z-[-1]"></div>
+            <div className="edit-mode-line bottom-left z-[-1]"></div>
+          </>
+        )}
 
-          <AnimatePresence>
-            {isPageEditing && (
-              <motion.div
-                initial={{ right: -20, opacity: 0 }}
-                animate={{ right: 0, opacity: 1 }}
-                exit={{ right: -20, opacity: 0 }}
-                transition={{ duration: 0.2 }}
-                className="absolute top-0 z-50 w-full flex justify-end px-4 py-3"
-              >
-                <div className="flex items-center justify-end gap-2">
-                  <Button variant="outline" className="gap-2">
-                    <Image src={Phone} alt="Phone" />
-                    <span>Device Preview</span>
-                  </Button>
-                  <Button
-                    variant="outline"
-                    onClick={() => {
-                      dispatch(setEditingMode(false));
-                    }}
-                    className="text-[#B91C1C]"
-                  >
-                    Close
-                  </Button>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
+        <div className="w-full h-full bg-[#FAFAFA] rounded-lg">
+          <div className="w-full relative">
+            <AnimatePresence>
+              {isPageEditing && (
+                <motion.div
+                  initial={{ top: -20, opacity: 0 }}
+                  animate={{ top: [-20, -20, 40, 40, 0], opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.4 }}
+                  className="absolute left-0 z-50 w-full flex justify-center"
+                >
+                  <div className="bg-[#A855F7] rounded-b-lg flex items-center justify-center px-4 py-[3px]">
+                    <span className="text-xs text-white font-medium">
+                      Edit Mode
+                    </span>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
 
-          {profileBackground && (
-            <div className="absolute top-0 left-0 w-full z-0">
-              <ProfileBackground
-                backgroundColor={profileBackground.backgroundColor}
-                borderColors={profileBackground.borderColors}
-              />
-            </div>
-          )}
-          <div className="w-full *:pt-[7.5rem]">
-            <div className="w-full container">
-              <div className="w-full flex items-end justify-between">
-                <div className="flex flex-col w-full">
-                  <div className="w-[8.563rem] h-[8.563rem] relative">
-                    <Image
-                      src={pageUser?.image || DefaultImage}
-                      alt={pageUser?.name || 'User Image'}
-                      fill
-                      className="object-bottom object-cover shadow-profile-image shadow-[#B32FAA12] border-white border-[5px] rounded-[16px]"
-                    />
+            <AnimatePresence>
+              {isPageEditing && (
+                <motion.div
+                  initial={{ right: -20, opacity: 0 }}
+                  animate={{ right: 0, opacity: 1 }}
+                  exit={{ right: -20, opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute top-0 z-50 w-full flex justify-end px-4 py-3"
+                >
+                  <div className="flex items-center justify-end gap-2">
+                    <Button variant="outline" className="gap-2">
+                      <Image src={Phone} alt="Phone" />
+                      <span>Device Preview</span>
+                    </Button>
+                    <Button
+                      variant="outline"
+                      onClick={() => {
+                        dispatch(setEditingMode(false));
+                      }}
+                      className="text-[#B91C1C]"
+                    >
+                      Close
+                    </Button>
                   </div>
-                  <div className="mt-[19px] flex flex-col items-start gap-3">
-                    {(pageUser?.userPage?.location || isOwner) && (
-                      <div className="flex items-center gap-[6px] h-6">
-                        <Image src={Location} alt="Location" />
-                        {isOwner && isPageEditing ? (
-                          <InlineEdit
-                            viewText={
-                              pageUser?.userPage?.location || 'Add Location'
-                            }
-                            value={pageUser?.userPage?.location || undefined}
-                            onChange={(e) => handleChange('location', e)}
-                            placeholder="Add Location"
-                          />
-                        ) : (
-                          <span className="text-base text-[#808080]">
-                            {pageUser?.userPage?.location}
-                          </span>
-                        )}
-                      </div>
-                    )}
-                    {(pageUser?.name || isOwner) && (
-                      <div className="flex items-center gap-3 h-8">
-                        {isOwner && isPageEditing ? (
-                          <InlineEdit
-                            viewText={pageUser?.name || 'Add Name'}
-                            value={pageUser?.name || undefined}
-                            onChange={(e) => handleChange('name', e)}
-                            viewElement={
-                              <span className="text-2xl font-medium text-[#1A1A1A]">
-                                {pageUser?.name}
-                              </span>
-                            }
-                            inputStyle="w-full text-2xl font-medium text-[#1A1A1A] items-center"
-                            placeholder="Add Name"
-                          />
-                        ) : (
-                          <span className="text-2xl font-medium text-[#1A1A1A]">
-                            {pageUser?.name}
-                          </span>
-                        )}
-                        {pageUser?.userPage?.contactEmail ||
-                          (isOwner && isPageEditing && (
-                            <SocialMediaButton
-                              type="email"
-                              isOpenable={isOwner && isPageEditing}
-                              url="#"
-                            />
-                          ))}
-                      </div>
-                    )}
-                    {(pageUser?.userPage?.biography || isOwner) && (
-                      <div className="flex items-center gap-[6px] h-6">
-                        {isOwner && isPageEditing ? (
-                          <InlineEdit
-                            viewText={
-                              pageUser?.userPage?.biography || 'Add Biography'
-                            }
-                            value={pageUser?.userPage?.biography || undefined}
-                            onChange={(e) => handleChange('biography', e)}
-                            placeholder="Add Biography"
-                          />
-                        ) : (
-                          <span className="text-base text-[#808080]">
-                            {pageUser?.userPage?.biography}
-                          </span>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className="w-full h-full min-h-[156px] flex flex-col items-end justify-between">
-                  {isOwner && !isPageEditing ? (
-                    <div className="flex items-center gap-2">
-                      <Button
-                        variant="outline"
-                        onClick={() => {
-                          if (isOwner) {
-                            dispatch(setEditingMode(true));
-                          }
-                        }}
-                      >
-                        Edit Profile
-                      </Button>
-                      <Button variant="outline" className="w-9 h-9" size="icon">
-                        <Settings stroke="#333" />
-                      </Button>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            {profileBackground && (
+              <div className="absolute top-0 left-0 w-full z-0">
+                <ProfileBackground
+                  backgroundColor={profileBackground.backgroundColor}
+                  borderColors={profileBackground.borderColors}
+                />
+              </div>
+            )}
+            <div className="w-full *:pt-[7.5rem]">
+              <div className="w-full container">
+                <div className="w-full flex items-end justify-between">
+                  <div className="flex flex-col w-full">
+                    <div className="w-[8.563rem] h-[8.563rem] relative">
+                      <Image
+                        src={pageUser?.image || DefaultImage}
+                        alt={pageUser?.name || 'User Image'}
+                        fill
+                        className="object-bottom object-cover shadow-profile-image shadow-[#B32FAA12] border-white border-[5px] rounded-[16px]"
+                      />
                     </div>
-                  ) : (
-                    <div></div>
-                  )}
-                  <div className="flex items-start gap-x-2">
-                    <SocialMediaButton
-                      type="github"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
-                    <SocialMediaButton
-                      type="x"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
-                    <SocialMediaButton
-                      type="youtube"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
-                    <SocialMediaButton
-                      type="instagram"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
-                    <SocialMediaButton
-                      type="dribbble"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
-                    <SocialMediaButton
-                      type="linkedin"
-                      url="#"
-                      isOpenable={isOwner && isPageEditing}
-                    />
+                    <div className="mt-[19px] flex flex-col items-start gap-3">
+                      {(pageUser?.userPage?.location || isOwner) && (
+                        <div className="flex items-center gap-[6px] h-6">
+                          <Image src={Location} alt="Location" />
+                          {isOwner && isPageEditing ? (
+                            <InlineEdit
+                              viewText={
+                                pageUser?.userPage?.location || 'Add Location'
+                              }
+                              value={pageUser?.userPage?.location || undefined}
+                              onChange={(e) => handleChange('location', e)}
+                              placeholder="Add Location"
+                            />
+                          ) : (
+                            <span className="text-base text-[#808080]">
+                              {pageUser?.userPage?.location}
+                            </span>
+                          )}
+                        </div>
+                      )}
+                      {(pageUser?.name || isOwner) && (
+                        <div className="flex items-center gap-3 h-8">
+                          {isOwner && isPageEditing ? (
+                            <InlineEdit
+                              viewText={pageUser?.name || 'Add Name'}
+                              value={pageUser?.name || undefined}
+                              onChange={(e) => handleChange('name', e)}
+                              viewElement={
+                                <span className="text-2xl font-medium text-[#1A1A1A]">
+                                  {pageUser?.name}
+                                </span>
+                              }
+                              inputStyle="w-full text-2xl font-medium text-[#1A1A1A] items-center"
+                              placeholder="Add Name"
+                            />
+                          ) : (
+                            <span className="text-2xl font-medium text-[#1A1A1A]">
+                              {pageUser?.name}
+                            </span>
+                          )}
+                          {pageUser?.userPage?.contactEmail ||
+                            (isOwner && isPageEditing && (
+                              <SocialMediaButton
+                                type="email"
+                                isOpenable={isOwner && isPageEditing}
+                                url="#"
+                              />
+                            ))}
+                        </div>
+                      )}
+                      {(pageUser?.userPage?.biography || isOwner) && (
+                        <div className="flex items-center gap-[6px] h-6">
+                          {isOwner && isPageEditing ? (
+                            <InlineEdit
+                              viewText={
+                                pageUser?.userPage?.biography || 'Add Biography'
+                              }
+                              value={pageUser?.userPage?.biography || undefined}
+                              onChange={(e) => handleChange('biography', e)}
+                              placeholder="Add Biography"
+                            />
+                          ) : (
+                            <span className="text-base text-[#808080]">
+                              {pageUser?.userPage?.biography}
+                            </span>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <div className="w-full h-full min-h-[156px] flex flex-col items-end justify-between">
+                    {isOwner && !isPageEditing ? (
+                      <div className="flex items-center gap-2">
+                        <Button
+                          variant="outline"
+                          onClick={() => {
+                            if (isOwner) {
+                              dispatch(setEditingMode(true));
+                            }
+                          }}
+                        >
+                          Edit Profile
+                        </Button>
+                        <Button
+                          variant="outline"
+                          className="w-9 h-9"
+                          size="icon"
+                        >
+                          <Settings stroke="#333" />
+                        </Button>
+                      </div>
+                    ) : (
+                      <div></div>
+                    )}
+                    <div className="flex items-start gap-x-2">
+                      <SocialMediaButton
+                        type="github"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                      <SocialMediaButton
+                        type="x"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                      <SocialMediaButton
+                        type="youtube"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                      <SocialMediaButton
+                        type="instagram"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                      <SocialMediaButton
+                        type="dribbble"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                      <SocialMediaButton
+                        type="linkedin"
+                        url="#"
+                        isOpenable={isOwner && isPageEditing}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,10 +68,33 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'horizontal-edit-mode-line-move': {
+          '0%': {
+            width: '0',
+            height: '16px',
+          },
+          '100%': {
+            width: '50%',
+            height: '16px',
+          },
+        },
+        'vertical-edit-mode-line-move': {
+          '0%': {
+            width: '16px',
+            height: '0px',
+          },
+          '100%': {
+            width: '16px',
+            height: '100%',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'edit-mode-top': 'horizontal-edit-mode-line-move 0.5s 0.5s forwards',
+        'edit-mode-vertical': 'vertical-edit-mode-line-move 0.5s 0.9s forwards',
+        'edit-mode-bottom': 'horizontal-edit-mode-line-move 0.5s 1.4s forwards',
       },
       boxShadow: {
         'project-card': '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
@@ -80,6 +103,8 @@ const config = {
         'social-media-icon': '0px 1px 2px 0px rgba(16, 24, 40, 0.05);',
         'profile-image':
           '0px 1px 10px 0px rgba(177, 128, 255, 0.12), 0px 1px 2px 0px rgba(177, 128, 255, 0.18);',
+        'edit-mode':
+          '0px 1px 4px 0px rgba(131, 95, 237, 0.45), 0px 0px 7px 5px rgba(131, 95, 237, 0.16);',
       },
     },
   },
