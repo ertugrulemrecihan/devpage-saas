1a9e1148ec9f3d51a9d453fbf3944c8fae5d17ee
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2024-08-23 00:05:42 +0300

    Update email sender in mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,6 +3,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const fromEmail = process.env.RESEND_FROM_EMAIL as string;
 
 export const sendVerificationEmail = async (
   email: string,
@@ -16,7 +17,7 @@ export const sendVerificationEmail = async (
   }
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: fromEmail,
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
@@ -27,7 +28,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: fromEmail,
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
